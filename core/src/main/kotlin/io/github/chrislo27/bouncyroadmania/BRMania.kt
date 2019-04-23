f097d50d722696f8e2b65c53f60e918d09f1e929
chrislo27 <kinchrislo@gmail.com>
2019-04-22 20:33:49 -0700

    Fix title

--- core/src/main/kotlin/io/github/chrislo27/bouncyroadmania/BRMania.kt
+++ core/src/main/kotlin/io/github/chrislo27/bouncyroadmania/BRMania.kt
@@ -5,7 +5,7 @@ import io.github.chrislo27.toolboks.version.Version
 
 object BRMania {
 
-    const val TITLE = "Crossing"
+    const val TITLE = "Bouncy Road Mania"
     val VERSION: Version = Version(0, 1, 0, "DEVELOPMENT")
     const val WIDTH = 1280
     const val HEIGHT = 720
