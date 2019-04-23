92e9d47e0288e546fc7c9a583377e67458b82655
chrislo27 <kinchrislo@gmail.com>
2019-04-22 21:19:40 -0700

    Begin engine work

---
+++ core/src/main/kotlin/io/github/chrislo27/bouncyroadmania/engine/BouncyRoad.kt
@@ -0,0 +1,28 @@
+package io.github.chrislo27.bouncyroadmania.engine
+
+import com.badlogic.gdx.Gdx
+import com.badlogic.gdx.graphics.OrthographicCamera
+import com.badlogic.gdx.graphics.Texture
+import com.badlogic.gdx.graphics.g2d.SpriteBatch
+import io.github.chrislo27.bouncyroadmania.renderer.PaperProjection
+import io.github.chrislo27.toolboks.registry.AssetRegistry
+
+
+class BouncyRoad {
+
+    val camera: OrthographicCamera = OrthographicCamera().apply {
+        setToOrtho(false, 1280f, 720f)
+    }
+    val projector = PaperProjection(2f)
+
+    fun render(batch: SpriteBatch) {
+        camera.update()
+        batch.projectionMatrix = camera.combined
+        batch.begin()
+        batch.draw(AssetRegistry.get<Texture>("tex_gradient"), 0f, 0f, Gdx.graphics.width.toFloat(),
+                Gdx.graphics.height.toFloat())
+        projector.render(batch)
+        batch.end()
+    }
+
+}
