2c7fb4e37250cb38f047aada091988a64b2bea17
chrislo27 <kinchrislo@gmail.com>
2019-04-26 08:56:07 -0700

    Move bouncer init to engine

--- core/src/main/kotlin/io/github/chrislo27/bouncyroadmania/engine/Engine.kt
+++ core/src/main/kotlin/io/github/chrislo27/bouncyroadmania/engine/Engine.kt
@@ -4,6 +4,7 @@ import com.badlogic.gdx.Gdx
 import com.badlogic.gdx.graphics.OrthographicCamera
 import com.badlogic.gdx.graphics.Texture
 import com.badlogic.gdx.graphics.g2d.SpriteBatch
+import com.badlogic.gdx.math.MathUtils
 import io.github.chrislo27.bouncyroadmania.engine.clock.Clock
 import io.github.chrislo27.bouncyroadmania.renderer.PaperProjection
 import io.github.chrislo27.toolboks.registry.AssetRegistry
@@ -17,6 +18,36 @@ class Engine(val clock: Clock) {
     val entities: MutableList<Entity> = mutableListOf()
     val projector = PaperProjection(2f)
     var bouncers: List<Bouncer> = listOf()
+
+    fun addBouncers() {
+        entities.removeAll(bouncers)
+        bouncers = listOf()
+
+        val radius = 1200f
+        val bouncers = mutableListOf<Bouncer>()
+        for (i in -1..15) {
+            val angle = (180 * (i / 14f)) - 90
+            val bouncer: Bouncer = if (i == 13) {
+                RedBouncer(this)
+            } else if (i == 12) {
+                YellowBouncer(this)
+            } else {
+                Bouncer(this)
+            }
+
+            val x: Float = MathUtils.cosDeg(angle) * radius
+            val z: Float = (-MathUtils.sinDeg(angle) + 1) * 0.3f
+
+            bouncer.posY = ((-MathUtils.sinDeg(angle) + 1) / 2f * 440f) + 200
+
+            bouncer.posX = x
+            bouncer.posZ = z
+
+            entities += bouncer
+            bouncers += bouncer
+        }
+        this.bouncers = bouncers
+    }
 
     fun renderUpdate(delta: Float) {
         entities.forEach {
--- core/src/main/kotlin/io/github/chrislo27/bouncyroadmania/screen/TestEngineScreen.kt
+++ core/src/main/kotlin/io/github/chrislo27/bouncyroadmania/screen/TestEngineScreen.kt
@@ -5,7 +5,9 @@ import com.badlogic.gdx.Input
 import com.badlogic.gdx.graphics.GL20
 import com.badlogic.gdx.math.MathUtils
 import io.github.chrislo27.bouncyroadmania.BRManiaApp
-import io.github.chrislo27.bouncyroadmania.engine.*
+import io.github.chrislo27.bouncyroadmania.engine.Ball
+import io.github.chrislo27.bouncyroadmania.engine.Bouncer
+import io.github.chrislo27.bouncyroadmania.engine.Engine
 import io.github.chrislo27.bouncyroadmania.engine.clock.Clock
 import io.github.chrislo27.bouncyroadmania.engine.clock.Swing
 import io.github.chrislo27.bouncyroadmania.engine.clock.tempo.TempoChange
@@ -30,30 +32,7 @@ class TestEngineScreen(main: BRManiaApp) : ToolboksScreen<BRManiaApp, TestEngine
         clock.tempos.add(TempoChange(clock.tempos, 0f, 154f, Swing.STRAIGHT, 0f))
         sendBallCycle = 0
 
-        val radius = 1200f
-        val bouncers = mutableListOf<Bouncer>()
-        for (i in -1 until 15 + 1) {
-            val angle = (180 * (i / 14f)) - 90
-            val bouncer: Bouncer = if (i == 13) {
-                RedBouncer(engine)
-            } else if (i == 12) {
-                YellowBouncer(engine)
-            } else {
-                Bouncer(engine)
-            }
-
-            val x: Float = MathUtils.cosDeg(angle) * radius
-            val z: Float = (-MathUtils.sinDeg(angle) + 1) * 0.3f
-
-            bouncer.posY = ((-MathUtils.sinDeg(angle) + 1) / 2f * 440f) + 200
-
-            bouncer.posX = x
-            bouncer.posZ = z
-
-            engine.entities += bouncer
-            bouncers += bouncer
-        }
-        engine.bouncers = bouncers
+        engine.addBouncers()
     }
 
     override fun render(delta: Float) {
