da1c4fd3f52cf4762a5f5534646588409e9aca65
chrislo27 <kinchrislo@gmail.com>
2019-05-11 21:15:24 -0700

    Change music credit to use https

--- core/src/main/kotlin/io/github/chrislo27/bouncyroadmania/util/MusicCredit.kt
+++ core/src/main/kotlin/io/github/chrislo27/bouncyroadmania/util/MusicCredit.kt
@@ -5,6 +5,6 @@ object MusicCredit {
     fun credit(vararg songTitle: String): String {
         return """${songTitle.joinToString(separator = ", ") { "\"$it\""}} Kevin MacLeod (incompetech.com)
 Licensed under Creative Commons: By Attribution 3.0 License
-http://creativecommons.org/licenses/by/3.0/"""
+https://creativecommons.org/licenses/by/3.0/"""
     }
 }
