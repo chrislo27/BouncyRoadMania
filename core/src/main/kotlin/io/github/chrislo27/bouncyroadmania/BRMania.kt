061bdaa13ff74711e5ee6eb08141820f94e822d4
chrislo27 <kinchrislo@gmail.com>
2019-05-19 16:27:49 -0700

    DEV version

--- core/src/main/kotlin/io/github/chrislo27/bouncyroadmania/BRMania.kt
+++ core/src/main/kotlin/io/github/chrislo27/bouncyroadmania/BRMania.kt
@@ -8,7 +8,7 @@ import io.github.chrislo27.toolboks.version.Version
 object BRMania {
 
     const val TITLE = "Bouncy Road Mania"
-    val VERSION: Version = Version(0, 1, 0, "")
+    val VERSION: Version = Version(0, 2, 0, "DEVELOPMENT")
     const val WIDTH = 1280
     const val HEIGHT = 720
     val BRMANIA_FOLDER: FileHandle by lazy { (Gdx.files.external(".bouncyroadmania/")).apply(FileHandle::mkdirs) }
