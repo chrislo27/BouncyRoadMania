17f85727bb9cef2e7fef18a40ac3a6f196945e2d
chrislo27 <kinchrislo@gmail.com>
2019-05-19 18:33:07 -0700

    Reduce score weights

--- core/src/main/kotlin/io/github/chrislo27/bouncyroadmania/engine/input/InputScore.kt
+++ core/src/main/kotlin/io/github/chrislo27/bouncyroadmania/engine/input/InputScore.kt
@@ -2,5 +2,5 @@ package io.github.chrislo27.bouncyroadmania.engine.input
 
 
 enum class InputScore(val weight: Float) {
-    ACE(1.0f), GOOD(0.9f), BARELY(0.7f), MISS(0f)
+    ACE(1.0f), GOOD(0.85f), BARELY(0.6f), MISS(0f)
 }
